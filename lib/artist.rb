c2d54b258397fee2697739be2b84752ebbfe5825
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 05:19:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,20 @@ class Artist
   
   attr_accessor :name, :songs
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
   
   def add_song(song)
     songs << song
+  end
+  
+  def self.all
+    @@all
   end
   
 end
